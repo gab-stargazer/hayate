528c5071d23bb8dd8278c1380e0af0fc52c6a807
lelestacia <km8003296@gmail.com>
2023-11-11 01:06:57 +0700

    Setup Dependencies (Not A Proper One)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.junit.adapter)
 }
 
 android {
@@ -52,19 +55,86 @@ android {
 
 dependencies {
 
+    //  Ktx
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
-    implementation(libs.activity.compose)
+
+    //  Coil
+    implementation(libs.coil)
+
+    //  Compose Activity
+    implementation(libs.compose.activity)
+
+    //  Compose BOM
     implementation(platform(libs.compose.bom))
-    implementation(libs.ui)
-    implementation(libs.ui.graphics)
-    implementation(libs.ui.tooling.preview)
-    implementation(libs.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
     androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.ui.test.junit4)
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
+
+    //  Compose
+    implementation(libs.compose.material.design)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphic)
+    implementation(libs.compose.ui.tooling)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.icon)
+    implementation(libs.compose.font)
+
+    //  Coroutine
+    implementation(libs.coroutine)
+
+    //  Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.crashlytic)
+
+    //  Hilt
+    implementation(libs.hilt)
+    implementation(libs.hilt.compose)
+    ksp(libs.hilt.compiler)
+
+    //  Logging Interceptor
+    implementation(libs.logging.interceptor)
+
+    //  Navigation
+    implementation(libs.navigation)
+
+    //  Paging
+    implementation(libs.paging)
+    implementation(libs.paging.compose)
+
+    //  Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi)
+    implementation(libs.moshi)
+    ksp(libs.moshi.codegen)
+
+    //  Room
+    implementation(libs.room)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+
+    //  Timber
+    implementation(libs.timber)
+
+    //  Ui Test
+    androidTestImplementation(libs.ui.test.espresso.core)
+    androidTestImplementation(libs.ui.test.android.junit)
+    androidTestImplementation(libs.ui.test.compose.junit)
+    androidTestImplementation(libs.ui.test.compose.manifest)
+
+    //  ViewModel
+    implementation(libs.lifecycle.ktx)
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.lifecycle.viewmodel.compose)
+
+    /*============================ Testing ==============================*/
+    // (Required) Writing and executing Unit Tests on the JUnit Platform
+    testImplementation(libs.junit)
+    testRuntimeOnly(libs.junit.engine)
+
+    // (Optional) If you need "Parameterized Tests"
+    testImplementation(libs.junit.parameterized)
+
+    // (Optional) If you also have JUnit 4-based tests
+    testImplementation(libs.test.junit.old)
+    testRuntimeOnly(libs.junit.vintage.engine)
+    /*====================================================================*/
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.kotlin.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.junit.adapter) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
